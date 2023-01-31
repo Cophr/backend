d49305bd9eb9f984f485340db36cf219faaf8e10
a20688392 <a20688392@gmail.com>
2023-02-01 02:11:05 +0800

    refactor: apply database environment variables

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,12 +1,14 @@
+import * as dotenv from "dotenv";
 import { DataSource, DataSourceOptions } from "typeorm";
 
+dotenv.config();
 export const dataSourceOptions: DataSourceOptions = {
   type: "mysql",
-  host: "",
-  port: 3306,
-  username: "",
-  password: "",
-  database: "",
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT, 10),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
   entities: [__dirname + "/../**/*.entity.js"],
   extra: {
     charset: "utf8mb4_unicode_ci",
