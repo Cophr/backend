efe390c347e67ba4a304b33852466a5632a41144
a20688392 <a20688392@gmail.com>
2023-02-01 22:27:14 +0800

    refactor: setup swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
+import { INestApplication } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import * as dotenv from "dotenv";
 
 import { AppModule } from "./app.module";
 
+dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  setupSwagger(app);
   await app.listen(3000);
 }
+
+function setupSwagger(app: INestApplication) {
+  const builder = new DocumentBuilder();
+  const config = builder
+    .setTitle(process.env.APP_SWAGGER_Title)
+    .setDescription(process.env.APP_SWAGGER_Description)
+    .setVersion(process.env.APP_SWAGGER_Version)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+}
+
 bootstrap();
