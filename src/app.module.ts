acec98e47f23619076461b0af10e7aa640d45a4e
a20688392 <a20688392@gmail.com>
2023-02-01 22:11:45 +0800

    refactor: setup env validate

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,19 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { TypeOrmModule } from "@nestjs/typeorm";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { dataSourceOptions } from "./config/data-source";
+import { validate } from "./config/env.validation";
 
 @Module({
-  imports: [TypeOrmModule.forRoot(dataSourceOptions)],
+  imports: [
+    ConfigModule.forRoot({
+      validate,
+    }),
+    TypeOrmModule.forRoot(dataSourceOptions),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/config/env.validation.ts
@@ -0,0 +1,42 @@
+import { plainToInstance } from "class-transformer";
+import { IsNotEmpty, IsNumber, IsString, validateSync } from "class-validator";
+
+class EnvironmentVariables {
+  @IsString()
+  @IsNotEmpty()
+  DB_HOST: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  DB_PORT: number;
+
+  @IsString()
+  @IsNotEmpty()
+  DB_USERNAME: string;
+
+  @IsString()
+  @IsNotEmpty()
+  DB_PASSWORD: string;
+
+  @IsString()
+  @IsNotEmpty()
+  DB_DATABASE: string;
+
+  @IsString()
+  @IsNotEmpty()
+  DB_TIMEZONE: string;
+}
+
+export function validate(config: Record<string, unknown>) {
+  const validatedConfig = plainToInstance(EnvironmentVariables, config, {
+    enableImplicitConversion: true,
+  });
+  const errors = validateSync(validatedConfig, {
+    skipMissingProperties: false,
+  });
+
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+  return validatedConfig;
+}
