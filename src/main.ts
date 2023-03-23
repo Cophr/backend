e8425f059cb4fe204a9573f8d85d8f183b483de7
a20688392 <a20688392@gmail.com>
2023-03-24 03:23:28 +0800

    feat: apply logger and setup handle exception error

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,21 @@
 import { INestApplication } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import * as fs from "fs";
+import * as morgan from "morgan";
 
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(morgan("default", { stream: logStream }));
   setupSwagger(app);
   await app.listen(3000);
 }
+
+const logStream = fs.createWriteStream("api.log", {
+  flags: "a", // append
+});
 
 function setupSwagger(app: INestApplication) {
   const builder = new DocumentBuilder();
