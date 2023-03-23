0f9b94e4bea7b596e4985b0d227e1442dd8cdfe8
a20688392 <a20688392@gmail.com>
2023-03-23 11:21:22 +0800

    feat: setup validation swagger info

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -5,8 +5,13 @@ import {
   Post,
   UsePipes,
 } from "@nestjs/common";
+import {
+  ApiCreatedResponse,
+  ApiUnprocessableEntityResponse,
+} from "@nestjs/swagger";
 
 import { CreateUserDto } from "./dto/create-user.dto";
+import { CreateEntityUserError } from "./exceptions/create-error.exception";
 import { UsersService } from "./users.service";
 import { SETTINGS } from "./users.utils";
 
@@ -15,6 +20,13 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
   @Post()
   @UsePipes(SETTINGS.VALIDATION_PIPE)
+  @ApiCreatedResponse({
+    description: "使用者創建成功",
+  })
+  @ApiUnprocessableEntityResponse({
+    description: "使用者格式不符",
+    type: CreateEntityUserError,
+  })
   create(@Body() userDto: CreateUserDto) {
     const user = this.usersService.create(userDto);
     if (!user) {
