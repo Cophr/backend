e438e0bd96f28140dab45ff260293e2b46cd7baf
a20688392 <a20688392@gmail.com>
2023-03-24 21:32:44 +0800

    fix: change unmodified lang zh-tw to en

--- src/users/users.utils.ts
+++ src/users/users.utils.ts
@@ -4,8 +4,7 @@ const PASSWORD_RULE =
   /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
 
 const PASSWORD_RULE_MESSAGE =
-  " 應包含 1 個大寫字母、1 個小寫字母以及數字和特殊字符。";
-//Password should have 1 upper case, lowcase letter along with a number and special character.
+  "Password should have 1 upper case, lowcase letter along with a number and special character.";
 const VALIDATION_PIPE = new ValidationPipe({
   errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
   stopAtFirstError: false,
