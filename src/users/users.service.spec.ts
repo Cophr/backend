ca5cdced3e9e338369be6d4a58cdf49051d1116a
a20688392 <a20688392@gmail.com>
2023-06-07 03:00:43 +0800

    test: usersService 單元測試-創建使用者

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,19 +1,41 @@
+import { HttpStatus } from "@nestjs/common";
 import { Test, TestingModule } from "@nestjs/testing";
+import { getRepositoryToken, TypeOrmModule } from "@nestjs/typeorm";
+import { dataSourceJest } from "src/config/data-source";
 
+import { CreateUserDto } from "./dto/create-user.dto";
+import { UserEntity } from "./entities/user.entity";
 import { UsersService } from "./users.service";
 
 describe("UsersService", () => {
-  let service: UsersService;
+  let userService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      imports: [TypeOrmModule.forRoot(dataSourceJest)],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useValue: UserEntity, // 使用測試資料庫的 Repository
+        },
+      ],
     }).compile();
 
-    service = module.get<UsersService>(UsersService);
+    userService = module.get<UsersService>(UsersService);
   });
 
-  it("should be defined", () => {
-    expect(service).toBeDefined();
+  it("應該會創建 一個使用者", async () => {
+    const test_data: CreateUserDto = {
+      email: "jhon@gmail.com",
+      name: "displayname",
+      account: "account1",
+      password: "Password@123",
+    };
+    const user = await userService.create(test_data);
+
+    expect(user).toBeDefined();
+    expect(user.statusCode).toEqual(HttpStatus.CREATED);
+    expect(user.message).toEqual("創建成功");
   });
 });
