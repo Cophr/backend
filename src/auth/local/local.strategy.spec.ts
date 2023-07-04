69a3719bedd87c44e6265f4fb8bf9192593b9a3b
a20688392 <a20688392@gmail.com>
2023-07-04 23:25:48 +0800

    feat: make localStrategy test

---
+++ src/auth/local/local.strategy.spec.ts
@@ -0,0 +1,69 @@
+import { UnauthorizedException } from "@nestjs/common";
+import { type TestingModule, Test } from "@nestjs/testing";
+
+import { AuthService } from "../auth.service";
+import { LocalStrategy } from "./local.strategy";
+
+describe("LocalStrategy", () => {
+  let localStrategy: LocalStrategy;
+  let authService: AuthService;
+
+  beforeEach(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      providers: [
+        {
+          provide: AuthService,
+          useValue: {
+            // 模拟AuthService中的方法
+            validateUser: jest.fn(),
+          },
+        },
+        LocalStrategy,
+      ],
+    }).compile();
+
+    localStrategy = moduleRef.get<LocalStrategy>(LocalStrategy);
+    authService = moduleRef.get<AuthService>(AuthService);
+  });
+
+  it("should be defined", () => {
+    expect(localStrategy).toBeDefined();
+  });
+
+  it("should return user payload if user is valid", async () => {
+    const mockAccount = "test";
+    const mockPassword = "password";
+
+    const mockUser = {
+      email: "test@example.com",
+      id: 1,
+    };
+
+    jest
+      .spyOn(authService, "validateUser")
+      .mockImplementation(async () => mockUser);
+
+    const result = await localStrategy.validate(mockAccount, mockPassword);
+
+    expect(result).toEqual({
+      email: mockUser.email,
+      id: mockUser.id,
+    });
+  });
+
+  it("should throw UnauthorizedException if user is invalid", async () => {
+    const mockAccount = "test";
+    const mockPassword = "password";
+
+    jest
+      .spyOn(authService, "validateUser")
+      .mockImplementation(async () => null);
+
+    try {
+      await localStrategy.validate(mockAccount, mockPassword);
+      fail("Expected to throw UnauthorizedException");
+    } catch (error) {
+      expect(error).toBeInstanceOf(UnauthorizedException);
+    }
+  });
+});
