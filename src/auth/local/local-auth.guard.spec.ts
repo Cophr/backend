51c613c64fcd853b3cd6c560fae965e9fef7cf0f
a20688392 <a20688392@gmail.com>
2023-07-04 22:08:27 +0800

    feat: make local auth guard test

---
+++ src/auth/local/local-auth.guard.spec.ts
@@ -0,0 +1,92 @@
+import { type ExecutionContext, UnauthorizedException } from "@nestjs/common";
+import { PassportModule } from "@nestjs/passport";
+import { Test } from "@nestjs/testing";
+
+import { AuthService } from "../auth.service";
+import { LocalStrategy } from "./local.strategy";
+import { LocalAuthGuard } from "./local-auth.guard";
+
+describe("LocalAuthGuard", () => {
+  let localAuthGuard: LocalAuthGuard;
+  let authService: AuthService;
+
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [PassportModule],
+      providers: [
+        LocalAuthGuard,
+        {
+          provide: AuthService,
+          useValue: {
+            // 模拟AuthService中的方法
+            validateUser: jest.fn(),
+          },
+        },
+        LocalStrategy,
+      ],
+    }).compile();
+
+    localAuthGuard = moduleRef.get<LocalAuthGuard>(LocalAuthGuard);
+    authService = moduleRef.get<AuthService>(AuthService);
+  });
+
+  it("should be defined", () => {
+    expect(localAuthGuard).toBeDefined();
+  });
+
+  it("should return true if user is valid", async () => {
+    const mockUser = {
+      email: "test@example.com",
+      id: 1,
+    };
+
+    const mockRequest = {
+      body: {
+        account: "test",
+        password: "password",
+      },
+    };
+
+    const mockResponse = {};
+
+    const mockExecutionContext = {
+      switchToHttp: () => ({
+        getRequest: () => mockRequest,
+        getResponse: () => mockResponse,
+      }),
+    } as ExecutionContext;
+
+    jest
+      .spyOn(authService, "validateUser")
+      .mockImplementation(async () => mockUser);
+
+    const result = await localAuthGuard.canActivate(mockExecutionContext);
+
+    expect(result).toBe(true);
+  });
+
+  it("should return false if user is invalid", async () => {
+    const mockResponse = {};
+    const mockExecutionContext = {
+      switchToHttp: () => ({
+        getRequest: () => ({
+          body: {
+            account: "test",
+            password: "password",
+          },
+        }),
+        getResponse: () => mockResponse,
+      }),
+    } as ExecutionContext;
+
+    jest
+      .spyOn(authService, "validateUser")
+      .mockImplementation(async () => null);
+
+    try {
+      await localAuthGuard.canActivate(mockExecutionContext);
+    } catch (error) {
+      expect(error).toBeInstanceOf(UnauthorizedException);
+    }
+  });
+});
