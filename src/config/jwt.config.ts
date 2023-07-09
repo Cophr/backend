da452df74e15060adab9733d4ff86005cf39dcc2
a20688392 <a20688392@gmail.com>
2023-07-10 02:58:06 +0800

    refactor: set mock environment variable

--- src/config/jwt.config.ts
+++ src/config/jwt.config.ts
@@ -9,3 +9,12 @@ export const jwtConfig: JwtModuleAsyncOptions = {
     signOptions: { expiresIn: "1d" },
   }),
 };
+
+export const jwtConfigJest: JwtModuleAsyncOptions = {
+  imports: [ConfigModule],
+  inject: [ConfigService],
+  useFactory: (configService: ConfigService) => ({
+    secret: configService.get("appSecret"),
+    signOptions: { expiresIn: "1d" },
+  }),
+};
--- src/swagger/swagger.module.ts
+++ src/swagger/swagger.module.ts
@@ -1,14 +1,28 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import { JwtModule } from "@nestjs/jwt";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from "src/app.controller";
 import { AppService } from "src/app.service";
 import { AuthModule } from "src/auth/auth.module";
+import { JwtStrategy } from "src/auth/jwt/jwt.strategy";
 import { dataSourceJest } from "src/config/data-source";
+import jestConfig from "src/config/jest.config";
+import { jwtConfigJest } from "src/config/jwt.config";
 import { UserModule } from "src/user/user.module";
 
 @Module({
   controllers: [AppController],
-  imports: [TypeOrmModule.forRoot(dataSourceJest), UserModule, AuthModule],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot(dataSourceJest),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [jestConfig],
+    }),
+    UserModule,
+    AuthModule,
+    JwtModule.registerAsync(jwtConfigJest),
+  ],
+  providers: [AppService, JwtStrategy],
 })
 export class SwaggerGenerateModule {}
