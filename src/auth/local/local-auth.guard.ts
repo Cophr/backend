04ea6fda394cb368d0714f0e33649184fa4fbf53
a20688392 <a20688392@gmail.com>
2023-08-09 23:51:28 +0800

    feat: add validation field information in LocalAuthGuard

--- src/auth/local/local-auth.guard.ts
+++ src/auth/local/local-auth.guard.ts
@@ -1,5 +1,32 @@
-import { Injectable } from "@nestjs/common";
+import {
+  type ArgumentMetadata,
+  type BadRequestException,
+  type ExecutionContext,
+  Injectable,
+} from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
+import { plainToClass } from "class-transformer";
+import { validationPipe } from "src/pipes/validation-pipe";
+import { LoginUserDto } from "src/user/dto/login-user.dto";
 
 @Injectable()
-export class LocalAuthGuard extends AuthGuard("local") {}
+export class LocalAuthGuard extends AuthGuard("local") {
+  async canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest<Request>();
+    const loginDto = plainToClass(LoginUserDto, request.body);
+
+    const metadata: ArgumentMetadata = {
+      data: "@Body()",
+      metatype: LoginUserDto,
+      type: "body",
+    };
+
+    await validationPipe
+      .transform(loginDto, metadata)
+      .catch((error: BadRequestException) => {
+        throw error;
+      });
+
+    return super.canActivate(context) as Promise<boolean> | boolean;
+  }
+}
