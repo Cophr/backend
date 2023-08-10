884f2fdda927d3e0766c759d49d47b8f0f6f89f8
a20688392 <a20688392@gmail.com>
2023-08-10 17:45:24 +0800

    test: add a user found in UserService

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -2,6 +2,7 @@ import { HttpStatus } from "@nestjs/common";
 import { type TestingModule, Test } from "@nestjs/testing";
 import { getRepositoryToken, TypeOrmModule } from "@nestjs/typeorm";
 import { dataSourceJest } from "src/config/data-source";
+import { type Repository } from "typeorm";
 
 import type { CreateUserDto } from "./dto/create-user.dto";
 import { UserEntity } from "./entities/user.entity";
@@ -9,6 +10,7 @@ import { UserService } from "./user.service";
 
 describe("UserService", () => {
   let userService: UserService;
+  let userRepository: Repository<UserEntity>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -24,6 +26,9 @@ describe("UserService", () => {
     }).compile();
 
     userService = module.get<UserService>(UserService);
+    userRepository = module.get<Repository<UserEntity>>(
+      getRepositoryToken(UserEntity),
+    );
   });
 
   it("應該會創建 一個使用者", async () => {
@@ -38,5 +43,29 @@ describe("UserService", () => {
     expect(user).toBeDefined();
     expect(user.statusCode).toEqual(HttpStatus.CREATED);
     expect(user.message).toEqual("創建成功");
+  });
+
+  it("should be found a user", async () => {
+    const account = "test";
+    const mockUser: Partial<UserEntity> = {
+      account: "test",
+      email: "test@example.com",
+      id: 1,
+      name: "test",
+      password: "$2b$05$zc4SaUDmE68OgrabgSoLX.CDMHZ8SD/aDeuJc7rxKmtqjP5WpH.Me",
+    };
+
+    jest
+      .spyOn(userRepository, "findOne")
+      .mockResolvedValue(mockUser as UserEntity);
+
+    const user = await userService.findOne(account);
+
+    expect(user).toBeDefined();
+    expect(user).toEqual(mockUser);
+  });
+
+  afterEach(async () => {
+    await userRepository.clear();
   });
 });
